8462327a354840e30143ee803e5bfcb535176f85
Yuan Tang <yyt5070@psu.edu>
2014-11-01 01:12:41 -0400

    added Application.java to initialize Parse(include API key)

---
+++ app/src/main/java/com/ryan/snapshot/Application.java
@@ -0,0 +1,41 @@
+package com.ryan.snapshot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.Parse;
+
+
+public class Application extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Parse.initialize(this, "aDaI7FmzfiFcgoHucvC2OEDzqiGsC2n0bcoFVsh4", "YzcvNDwvKfkjpjRnZJKUWxlmxrQHhmcavZkgFzFy");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_application, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
