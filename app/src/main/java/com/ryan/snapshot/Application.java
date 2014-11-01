4a6b88d15bfbeee02cb49264373ca5b14ff31de4
Yuan Tang <yyt5070@psu.edu>
2014-11-01 04:51:12 -0400

    fixed login layout

--- app/src/main/java/com/ryan/snapshot/Application.java
+++ app/src/main/java/com/ryan/snapshot/Application.java
@@ -14,8 +14,7 @@ public class Application extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Parse.initialize(this, "aDaI7FmzfiFcgoHucvC2OEDzqiGsC2n0bcoFVsh4", "YzcvNDwvKfkjpjRnZJKUWxlmxrQHhmcavZkgFzFy");
-
+        Parse.initialize(this, "w5Ub5y19f06MIZNdGvUoIIMW4rUx891eTRR4m8jp", "WYJKiIspLdFO7OmFNUlaIZjKgUjHWiPMOuSpoNr5");
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
         testObject.saveInBackground();
