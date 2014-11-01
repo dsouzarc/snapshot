7a216a5be5709bcf8fb2a6d03b2605ac47df7d74
Ryan D'souza <dsouzarc@gmail.com>
2014-11-01 04:26:18 -0400

    Completely cleaned up code.

--- app/src/main/java/com/ryan/snapshot/LiveGame.java
+++ app/src/main/java/com/ryan/snapshot/LiveGame.java
@@ -1,26 +1,26 @@
 package com.ryan.snapshot;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.content.Intent;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.Result;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.RGBLuminanceSource;
-
-import com.ryan.snapshot.camera.IntentIntegrator;
-import android.widget.Button;
-import android.widget.Toast;
-import com.ryan.snapshot.camera.IntentResult;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.util.Log;
-public class LiveGame extends Activity {
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-    public static final String TAG = "com.ryan.snapshot";
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.ryan.snapshot.camera.IntentIntegrator;
+import com.ryan.snapshot.camera.IntentResult;
+
+public class LiveGame extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,48 +28,36 @@ public class LiveGame extends Activity {
         setContentView(R.layout.activity_live_game);
 
         final Button scanCode = (Button) findViewById(R.id.buttonScanQRCode);
-        scanCode.setOnClickListener(new android.view.View.OnClickListener() {
+        scanCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(android.view.View v) {
                 IntentIntegrator integrator = new IntentIntegrator(LiveGame.this);
                 integrator.initiateScan();
             }
         });
-        makeToast("Starting");
-        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), com.ryan.snapshot.R.drawable.qr_code_test);
 
-        try
-        {
-            makeToast("Here");
-            if (bitmap == null) {
-                Log.e(TAG, "uri is not a bitmap,");
-            }
-            int width = bitmap.getWidth(), height = bitmap.getHeight();
-            int[] pixels = new int[width * height];
+        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.qr_code_test);
+
+        try {
+            final int width = bitmap.getWidth(), height = bitmap.getHeight();
+            final int[] pixels = new int[width * height];
             bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
             bitmap.recycle();
-            RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
-            BinaryBitmap bBitmap = new BinaryBitmap(new HybridBinarizer(source));
-            MultiFormatReader reader = new MultiFormatReader();
-            makeToast("Down");
-            try
-            {
-                makeToast("Try");
-                Result result = reader.decode(bBitmap);
-                makeToast("Success");
-                Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_LONG).show();
-                makeToast("W???");
+
+            final RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
+            final BinaryBitmap bBitmap = new BinaryBitmap(new HybridBinarizer(source));
+            final MultiFormatReader reader = new MultiFormatReader();
+
+            try {
+                final Result result = reader.decode(bBitmap);
+                log(result.getText());
             }
-            catch (Exception e)
-            {
-                makeToast(e.toString());
-                Log.e(TAG, "decode exception", e);
+            catch (Exception e) {
+                log(e.toString());
             }
         }
-        catch (Exception e)
-        {
-            makeToast("Down: " + e.toString());
-            Log.e(TAG, "can not open file", e);
+        catch (Exception e) {
+            log(e.toString());
         }
     }
 
@@ -77,23 +65,24 @@ public class LiveGame extends Activity {
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
     }
 
+    private void log(final String text) {
+        Log.e("com.ryan.snapshot", text);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (scanResult != null) {
-
-            // handle scan result
             final String contantsString = scanResult.getContents()==null?"0":scanResult.getContents();
             if (contantsString.equalsIgnoreCase("0")) {
-                Toast.makeText(this, "Problem to get the  contant Number", Toast.LENGTH_LONG).show();
-
-            }else {
-                Toast.makeText(this, contantsString, Toast.LENGTH_LONG).show();
-
+                makeToast("No text gotten");
+            }
+            else {
+                makeToast(contantsString);
             }
         }
-        else{
-            Toast.makeText(this, "Problem to secan the barcode.", Toast.LENGTH_LONG).show();
+        else {
+            makeToast("Scanning problem");
         }
     }
 
--- app/src/main/java/com/ryan/snapshot/Lobby.java
+++ app/src/main/java/com/ryan/snapshot/Lobby.java
@@ -1,14 +1,14 @@
 package com.ryan.snapshot;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.content.Intent;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 
