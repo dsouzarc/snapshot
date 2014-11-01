d1ac7f71f84be0cefe2f717e9cb4d850593716ab
Ryan D'souza <dsouzarc@gmail.com>
2014-11-01 04:22:37 -0400

    Getting QR Code from stored photo working

--- app/src/main/java/com/ryan/snapshot/LiveGame.java
+++ app/src/main/java/com/ryan/snapshot/LiveGame.java
@@ -5,12 +5,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.Result;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.RGBLuminanceSource;
+
 import com.ryan.snapshot.camera.IntentIntegrator;
 import android.widget.Button;
 import android.widget.Toast;
 import com.ryan.snapshot.camera.IntentResult;
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 public class LiveGame extends Activity {
+
+    public static final String TAG = "com.ryan.snapshot";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +35,46 @@ public class LiveGame extends Activity {
                 integrator.initiateScan();
             }
         });
+        makeToast("Starting");
+        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), com.ryan.snapshot.R.drawable.qr_code_test);
 
+        try
+        {
+            makeToast("Here");
+            if (bitmap == null) {
+                Log.e(TAG, "uri is not a bitmap,");
+            }
+            int width = bitmap.getWidth(), height = bitmap.getHeight();
+            int[] pixels = new int[width * height];
+            bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+            bitmap.recycle();
+            RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
+            BinaryBitmap bBitmap = new BinaryBitmap(new HybridBinarizer(source));
+            MultiFormatReader reader = new MultiFormatReader();
+            makeToast("Down");
+            try
+            {
+                makeToast("Try");
+                Result result = reader.decode(bBitmap);
+                makeToast("Success");
+                Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_LONG).show();
+                makeToast("W???");
+            }
+            catch (Exception e)
+            {
+                makeToast(e.toString());
+                Log.e(TAG, "decode exception", e);
+            }
+        }
+        catch (Exception e)
+        {
+            makeToast("Down: " + e.toString());
+            Log.e(TAG, "can not open file", e);
+        }
+    }
+
+    private void makeToast(final String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
     }
 
     @Override
