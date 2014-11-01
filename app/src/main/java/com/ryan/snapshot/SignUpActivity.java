6db3a53d14399f75205f081941ba3dbbc944b71f
Yuan Tang <yyt5070@psu.edu>
2014-11-01 01:59:32 -0400

    attached fields in signupactivity.java with layout

--- app/src/main/java/com/ryan/snapshot/SignUpActivity.java
+++ app/src/main/java/com/ryan/snapshot/SignUpActivity.java
@@ -17,5 +17,23 @@ import com.parse.SignUpCallback;
  * Created by Yuan on 11/1/2014.
  */
 
-public class SignUpActivity {
+public class SignUpActivity extends Activity{
+
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected EditText mEmail;
+    protected Button mSignUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.activity_sign_up);
+
+        mUsername = (EditText)findViewById(R.id.usernameField);
+        mPassword = (EditText)findViewById(R.id.passwordField);
+        mEmail = (EditText)findViewById(R.id.emailField);
+        mSignUpButton = (Button)findViewById(R.id.signupButton);
+        
+    }
 }
