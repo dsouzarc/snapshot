d40d804f5ee67c166e5c47918cdbb361847cd16d
Ryan D'souza <dsouzarc@gmail.com>
2014-10-31 23:30:19 -0400

    New Activity for viewing game details before joining

---
+++ app/src/main/java/com/ryan/snapshot/LiveGame.java
@@ -0,0 +1,36 @@
+package com.ryan.snapshot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.ryan.snapshot.R;
+
+public class LiveGame extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_game);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.live_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/ryan/snapshot/SnapShot_API.java
@@ -0,0 +1,7 @@
+package com.ryan.snapshot;
+
+/**
+ * Created by Ryan on 10/31/14.
+ */
+public class SnapShot_API {
+}
