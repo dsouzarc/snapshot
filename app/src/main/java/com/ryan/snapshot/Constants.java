e5ccec652936f563c7b101c187b3d39ef58614a5
Ryan D'souza <dsouzarc@gmail.com>
2014-11-01 15:01:46 -0400

    Updated constants

--- app/src/main/java/com/ryan/snapshot/Constants.java
+++ app/src/main/java/com/ryan/snapshot/Constants.java
@@ -1,9 +1,12 @@
 package com.ryan.snapshot;
 
-/**
- * Created by Ryan on 11/1/14.
- */
+import android.util.Log;
+
 public class Constants {
 
     public static final String URL = "https://snapshot.azure-mobile.net/";
+
+    public static void log(final String message) {
+        Log.e("com.ryan.snapshot", message);
+    }
 }
