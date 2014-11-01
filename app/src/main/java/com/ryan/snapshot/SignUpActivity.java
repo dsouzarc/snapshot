a3d5b10a1559aa430d6e4ebb0c1a7ad12ae8a9ba
Yuan Tang <yyt5070@psu.edu>
2014-11-01 01:57:15 -0400

    added signupactivity.java

---
+++ app/src/main/java/com/ryan/snapshot/SignUpActivity.java
@@ -0,0 +1,21 @@
+package com.ryan.snapshot;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+/**
+ * Created by Yuan on 11/1/2014.
+ */
+
+public class SignUpActivity {
+}
